599d88fc142041bb3aac6435bbf7e319e69f5d34
PRBHUMKAR13 <p.r.bhumkar13@gmail.com>
2024-05-15 13:18:52 +0530

    Commited

---
+++ facebook_ui/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FacebookLoginPage());
+}
+
+class FacebookLoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 300),
+            //
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'facebook',
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 33, 102, 222),
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 40),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email address or phone number',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 49, 110, 215),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Forgotten account',
+                    style: TextStyle(
+                        color: const Color.fromARGB(255, 40, 105, 217)),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: const Color.fromARGB(255, 40, 105, 217)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
